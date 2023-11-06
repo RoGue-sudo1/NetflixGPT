ce0164319ca4187972b087350e1fa35c238ebcfb
Harsh Rathore <harshrathore7514@gmail.com>
2023-11-06 20:11:57 +0530

    configured the redux store and dispatched the action using firebse manage user api

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ Body
  - (login && sign up page )(authentication){
     -  For authetication we are using firebase
     -  We are pushing the user to the redux store
+    -  Now we are not dispatching action everytime we sign in or sign out(in other way we are not hard   coding it whenever we are signin and sign out ) rather than that we are managing users by a   firebase api.
+    -  This api tell us when the user signin and user sign out and we have to use this api once so we are using useEffect so whenver the user clicks on the sign iin or signout button the page re renders and useEffect will be called
+    -  We are using the api code from firebase at body level
  }
  -  Browse page ( if authenticated user)
        - Header with logo at right and at left home, browse,signout, user logo
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,26 +1,42 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Browse from "./Browse"
-import Login from "./Login"
+import { useDispatch } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import Browse from "./Browse";
+import Login from "./Login";
+import { auth } from "../utils/firebase";
+import { addUser, removeUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 function Body() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, displayName, email } = user;
+        dispatch(addUser({ uid: uid, email: email, name: displayName }));
+      } else {
+        dispatch(removeUser());
+      }
+    });
+  }, []);
 
   const appRouter = createBrowserRouter([
     {
-        path:'/',
-        element:<Login/>
+      path: "/",
+      element: <Login />,
     },
     {
-        path:'/browse',
-        element:<Browse/>
-    }
-  ])
-
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
 
   return (
     <div>
-      <RouterProvider router={appRouter}/>
+      <RouterProvider router={appRouter} />
     </div>
-  )
+  );
 }
 
-export default Body
+export default Body;
